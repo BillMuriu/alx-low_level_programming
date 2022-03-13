034cd960147c8194c661818da429e335e15abc07
Bill <njorogekamau63@gmail.com>
2022-03-12 23:45:08 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9) {
+		    putchar(',');
+		    putchar(' ');
+		}
+	}
+	putchar('\n');
+
+    return 0;
+}
