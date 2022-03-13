858bed2b9e64af2cf82e076ea46d5f2f0a20db01
Bill <njorogekamau63@gmail.com>
2022-03-12 23:38:08 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'a';
+    int i = 0;
+    
+	while (i < 16)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+		    putchar(ch);
+		    ch++;
+		}
+		i++;
+	}
+	putchar('\n');
+    return 0;
+}
