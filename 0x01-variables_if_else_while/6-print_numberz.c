d74f0f1fa63d74f2733b3f8ba1fb30266f4ac064
Bill <njorogekamau63@gmail.com>
2022-03-12 23:36:02 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-	return 0;
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
+
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch ='z';
+	while(ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return 0;
+}
