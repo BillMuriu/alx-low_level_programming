503b8fa3002c02886545347398dc8678c29d92af
Bill <njorogekamau63@gmail.com>
2022-03-14 19:25:27 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-	    for (int j = i + 1; j <= 9; j++)
-	    {
-	        putchar(i + '0');
-	        putchar(j + '0');
-	        if (((i * 10) + j) < 89)
-	        {
-	            putchar(',');
-                putchar(' ');
+	int t1 = 48;
+	int t2;
+	int com = 44;
 
-	        }
-	    }
+	while (t1 <= 57)
+	{
+		t2 = t1 + 1;
+		while (t2 <= 57)
+		{
+			putchar(t1);
+			putchar(t2);
+			if (t1 != 56 || t2 != 57)
+			{
+				putchar(com);
+				putchar(32);
+			}
+			t2++;
+		}
+		t1++;
 	}
 	putchar('\n');
-    return 0;
+	return (0);
 }
