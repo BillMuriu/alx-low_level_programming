bb5d88f99cd2b6c211adbbf7eecea59cc94ca797
Bill <njorogekamau63@gmail.com>
2022-03-14 19:28:32 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    int a, b, c;
-    
-	for (int i = 0; i <= 9; i++)
-	{
-	    for (int j = i + 1; j <= 9; j++)
-	    {
-	        for (int k = j + 1; k <= 9; k++)
-	        {
-	            putchar(i + '0');
-	            putchar(j + '0');
-	            putchar(k + '0');
-	            if (((i * 100) + (j * 10) + k) < 789)
-	            {
-	                putchar(',');
-                    putchar(' ');
+	int t1 = 48;
+	int t2, t3;
+	int com = 44;
 
-	            }
-	        }
-	    }
+	while (t1 <= 57)
+	{
+		t2 = t1 + 1;
+		while (t2 <= 57)
+		{
+			t3 = t2 + 1;
+			while (t3 <= 57)
+			{
+				putchar(t1);
+				putchar(t2);
+				putchar(t3);
+				if (t1 != 55 || t2 != 56 || t3 != 57)
+				{
+					putchar(com);
+					putchar(32);
+				}
+				t3++;
+			}
+			t2++;
+		}
+		t1++;
 	}
 	putchar('\n');
-    return 0;
+	return (0);
 }
