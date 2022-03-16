ec224cd56f3805d4b9a04a19a91d70cdfe1de2c7
Bill <njorogekamau63@gmail.com>
2022-03-16 03:29:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10
+ * void
+ * Return no value
+ */
+ void print_alphabet_x10(void)
+ {
+	 char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	 int i, j;
+
+	 i = 0;
+	 while (i < 10)
+         {
+		 j = 0;
+
+		 while (j < 26)
+		 {
+			 _putchar(alphabet[j]);
+			 j++;
+	         }
+		 _putchar('\n');
+		 i++;
+         }
+ }
