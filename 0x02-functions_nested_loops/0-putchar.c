3a723f8385e2f57498743c84f7dc217129359af1
Bill <njorogekamau63@gmail.com>
2022-03-16 01:25:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(char);
+int _putchar(char);
 
 #endif
