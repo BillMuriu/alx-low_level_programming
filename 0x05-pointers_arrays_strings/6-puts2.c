d54649eb68f1b5878f6218617bdb57b34f8f2f72
Bill <njorogekamau63@gmail.com>
2022-03-28 05:43:40 -0700

    #include main.h
    /**
    * puts2 - prints a string from the frirst char
    * @str: string variable
    *
    * Return: void
    */
    
    void puts2(char *str)
    {
    int i = 0;
    
    while (str[i] != '\0')
    {
    if (i % 2 == 0)
    {
    _putchar(str[i]);
    }
    i++;
    }
    _putchar('\n');
    }'

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 - prints a string from the frirst char
+* @str: string variable
+*
+* Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
